42465f0d66acaab85e897509a3977b50ca167170
Botond János Kovács <botondjanoskovacs@gmail.com>
2019-05-03 15:19:19 +0200

    Hopefully will get 1 or 2 points for this ...

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -34,6 +34,143 @@
 #include "framework.h"
 
 const int tessellationLevel = 20;
+const float epsilon = 0.001;
+const vec3 DINI_FORWARD = vec3 (0.0, 0.0, 1.0);
+
+
+inline float cscf (float a) {
+	return 1.0 / sinf (a);
+}
+inline float secf (float a) {
+	return 1.0 / cosf (a);
+}
+inline float frand () {
+	return (float) rand () / (float) RAND_MAX;
+}
+
+// Source: https://github.com/glslify/glsl-inverse/blob/master/index.glsl
+inline mat4 inverse(mat4 &m) {
+	float
+		a00 = m.m[0][0], a01 = m.m[0][1], a02 = m.m[0][2], a03 = m.m[0][3],
+		a10 = m.m[1][0], a11 = m.m[1][1], a12 = m.m[1][2], a13 = m.m[1][3],
+		a20 = m.m[2][0], a21 = m.m[2][1], a22 = m.m[2][2], a23 = m.m[2][3],
+		a30 = m.m[3][0], a31 = m.m[3][1], a32 = m.m[3][2], a33 = m.m[3][3],
+
+		b00 = a00 * a11 - a01 * a10,
+		b01 = a00 * a12 - a02 * a10,
+		b02 = a00 * a13 - a03 * a10,
+		b03 = a01 * a12 - a02 * a11,
+		b04 = a01 * a13 - a03 * a11,
+		b05 = a02 * a13 - a03 * a12,
+		b06 = a20 * a31 - a21 * a30,
+		b07 = a20 * a32 - a22 * a30,
+		b08 = a20 * a33 - a23 * a30,
+		b09 = a21 * a32 - a22 * a31,
+		b10 = a21 * a33 - a23 * a31,
+		b11 = a22 * a33 - a23 * a32,
+
+		det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+	return mat4(
+		(a11 * b11 - a12 * b10 + a13 * b09) / det,
+		(a02 * b10 - a01 * b11 - a03 * b09) / det,
+		(a31 * b05 - a32 * b04 + a33 * b03) / det,
+		(a22 * b04 - a21 * b05 - a23 * b03) / det,
+		(a12 * b08 - a10 * b11 - a13 * b07) / det,
+		(a00 * b11 - a02 * b08 + a03 * b07) / det,
+		(a32 * b02 - a30 * b05 - a33 * b01) / det,
+		(a20 * b05 - a22 * b02 + a23 * b01) / det,
+		(a10 * b10 - a11 * b08 + a13 * b06) / det,
+		(a01 * b08 - a00 * b10 - a03 * b06) / det,
+		(a30 * b04 - a31 * b02 + a33 * b00) / det,
+		(a21 * b02 - a20 * b04 - a23 * b00) / det,
+		(a11 * b07 - a10 * b09 - a12 * b06) / det,
+		(a00 * b09 - a01 * b07 + a02 * b06) / det,
+		(a31 * b01 - a30 * b03 - a32 * b00) / det,
+		(a20 * b03 - a21 * b01 + a22 * b00) / det
+    );
+}
+
+inline vec4 quaternion (vec3 axis, float angle) {
+	float s = sinf (angle / 2.0);
+	return vec4 (
+		axis.x * s,
+		axis.y * s,
+		axis.z * s,
+		cosf (angle / 2.0)
+	);
+}
+
+inline vec4 qmul (vec4 q1, vec4 q2) {
+	vec3 d1 = vec3 (q1.x, q1.y, q1.z);
+	vec3 d2 = vec3 (q2.x, q2.y, q2.z);
+	vec3 c = cross (d1, d2);
+	return vec4 (
+		d2.x * q1.w + d1.x * q2.w + c.x,
+		d2.y * q1.w + d1.y * q2.w + c.y,
+		d2.z * q1.w + d1.z * q2.w + c.z,
+		q1.w * q2.w - dot(d1, d2)
+	);
+}
+
+inline vec3 rotate (vec3 u, vec4 q) {
+	vec4 qinv = (-q.x, -q.y, -q.z, q.w);
+	vec4 qr = qmul (qmul (q, vec4 (u.x, u.y, u.z, 0)), qinv);
+	return vec3 (qr.x, qr.y, qr.z);
+}
+
+inline vec4 vecRotation (vec3 a, vec3 b) {
+	vec3 start = normalize (a);
+	vec3 dest = normalize (b);
+	float cosTheta = dot (start, dest);
+	vec3 rotationAxis;
+	if (cosTheta < -1.0 + epsilon) {
+		rotationAxis = cross (vec3 (0, 0, 1), start);
+		if (length (rotationAxis) < epsilon) {
+			rotationAxis = cross (vec3 (1, 0, 0), start);
+		}
+		rotationAxis = normalize (rotationAxis);
+		return quaternion (rotationAxis, M_PI);
+	}
+	rotationAxis = cross (start, dest);
+	float s = sqrtf ( (1.0 + cosTheta) * 2.0 );
+	float invS = 1.0 / s;
+	return vec4 (s * 0.5, rotationAxis.x * invS, rotationAxis.y * invS, rotationAxis.z * invS);
+}
+
+inline vec4 orient (vec3 forward, vec3 dir, vec3 up) {
+	vec3 right = cross (dir, up);
+	vec3 realUp = cross (right, dir);
+	vec4 rot1 = vecRotation (forward, dir);
+	vec3 newUp = rotate (vec3 (0, 1, 0), rot1);
+	vec4 rot2 = vecRotation (newUp, realUp);
+	return qmul (rot2, rot1);
+}
+
+mat4 qRotationMatrix (vec4 q) {
+	return mat4 (
+		// First row
+		q.x * q.x + q.y * q.y - q.z * q.z - q.w * q.w, 
+		2.0 * q.y * q.z - 2.0 * q.x * q.w,
+		2.0 * q.y * q.w + 2.0 * q.x * q.z,
+		0,
+
+		// Second row
+		2.0 * q.y * q.z + 2.0 * q.x * q.w,
+		q.x * q.x - q.y * q.y + q.z * q.z - q.w * q.w,
+		2.0 * q.z * q.w - 2.0 * q.x * q.y,
+		0,
+
+		// Third row
+		2.0 * q.y * q.w - 2.0 * q.x * q.z,
+		2.0 * q.z * q.w + 2.0 * q.x * q.y,
+		q.x * q.x - q.y * q.y - q.z * q.z + q.w * q.w,
+		0,
+
+		// Forth row
+		0, 0, 0, 1
+	);
+}
 
 struct Camera {
 
@@ -236,6 +373,11 @@ struct Vertex {
 	vec2 texCoord;
 };
 
+struct AABB {
+	vec3 center;
+	vec3 halfDimensions;
+};
+
 class Drawable {
 
 protected:
@@ -244,12 +386,17 @@ protected:
 
 	void AddToRenderState (RenderState &state) {
 
+		mat4 rot;
+		if (useQuaternion) {
+			rot = qRotationMatrix (rotationQuaternion);
+		} else {
+			rot = RotationMatrix (rotation, rotationAxis);
+		}
+
 		state.model = ScaleMatrix (scale)
-			* RotationMatrix (rotation, rotationAxis)
+			* rot
 			* TranslateMatrix (translate);
-		state.modelInverse = TranslateMatrix (-translate)
-			* RotationMatrix (-rotation, rotationAxis)
-			* ScaleMatrix (vec3 (1.0 / scale.x, 1.0 / scale.y, 1.0 / scale.z));
+		state.modelInverse = inverse (state.model);
 		state.modelViewProjection = state.model * state.view * state.projection;
 		state.texture = texture;
 		state.material = material;
@@ -264,9 +411,14 @@ public:
 	vec3 scale;
 	vec3 rotationAxis;
 	float rotation;
+	vec4 rotationQuaternion;
+	bool useQuaternion = false;
 	Shader* shader;
 	Texture* texture;
 	Material* material;
+	AABB boundingBox;
+
+	virtual void CalculateBoundingBox () = 0;
 
 	void Init () {
 
@@ -310,11 +462,7 @@ protected:
 	int numTriangleStrips;
 	int numVerticesPerStrip;
 
-	virtual float transU (float uIn) = 0;
-	virtual float transV (float vIn) = 0;
-
-	virtual vec3 surfPos (float u, float v) = 0;
-	virtual vec3 surfNormal (float u, float v) = 0;
+	std::vector <Vertex> vertices;
 
 	Vertex vertex (float u, float v) {
 
@@ -337,9 +485,58 @@ protected:
 
 public:
 
-	void Create (int uSubdivCount = 30, int vSubdivCount = 30) {
+	virtual float transU (float uIn) = 0;
+	virtual float transV (float vIn) = 0;
 
-		std::vector <Vertex> vertices;
+	virtual vec3 surfPos (float u, float v) = 0;
+	virtual vec3 surfNormal (float u, float v) = 0;
+
+	void CalculateBoundingBox () {
+
+		float minX = vertices [0].position.x;
+		float maxX = vertices [0].position.x;
+		float minY = vertices [0].position.y;
+		float maxY = vertices [0].position.y;
+		float minZ = vertices [0].position.z;
+		float maxZ = vertices [0].position.z;
+
+		mat4 model = ScaleMatrix (scale)
+			* RotationMatrix (rotation, rotationAxis)
+			* TranslateMatrix (translate);
+
+		for (int i = 1; i < vertices.size (); i++) {
+
+			vec4 vPos = vec4 (
+				vertices [i].position.x,
+				vertices [i].position.y,
+				vertices [i].position.z,
+				1.0
+			) * model;
+
+			if (minX > vPos.x) minX = vPos.x;
+			if (maxX < vPos.x) maxX = vPos.x;
+			if (minY > vPos.y) minY = vPos.y;
+			if (maxY > vPos.y) maxY = vPos.y;
+			if (minZ > vPos.z) minZ = vPos.z;
+			if (maxZ > vPos.z) maxZ = vPos.z;
+		}
+
+		boundingBox.center = vec3 (
+			(maxX + minX) / 2.0 + translate.x,
+			(maxY + minY) / 2.0 + translate.y,
+			(maxZ + minZ) / 2.0 + translate.z
+		);
+		boundingBox.halfDimensions = vec3 (
+			abs (maxX - minX) / 2.0,
+			abs (maxY - minY) / 2.0,
+			abs (maxZ - minZ) / 2.0
+		);
+
+	}
+
+	void Create (int uSubdivCount = 50, int vSubdivCount = 50) {
+
+		vertices.clear ();
 		numVerticesPerStrip = (vSubdivCount + 1) * 2;
 		numTriangleStrips = uSubdivCount;
 
@@ -380,7 +577,7 @@ public:
 
 class Sphere : public Surface {
 
-protected:
+public:
 	float transU (float u) {
 		return u * 2.0 * M_PI;
 	}
@@ -400,6 +597,151 @@ protected:
 	}
 
 };
+
+class DiniSurface : public Surface {
+
+public:
+
+	float a = 1.0;
+	float b = 0.15;
+
+	float transU (float u) {
+		return u * 4.0 * M_PI;
+	}
+	float transV (float v) {
+		return 0.01 + v * (1.0 - 0.01);
+	}
+
+	vec3 surfPos (float u, float v) {
+		return vec3 (
+			a * cosf (u) * sinf (v),
+			a * sinf (u) * sinf (v),
+			a * ( cosf (v) + logf (tanf (v / 2.0)) ) + b * u
+		);
+	}
+	vec3 surfNormal (float u, float v) {
+		vec3 du, dv;
+
+		du = vec3 (
+			-a * sinf (u) * sinf (v),
+			a * cosf (u) * sinf (v),
+			b
+		);
+		dv = vec3 (
+			a * cosf (u) * cosf (v),
+			a * sinf (u) * cosf (v),
+			0.5 * a * ( cscf (v / 2.0) * secf (v / 2.0) - 2.0 * sinf (v) )
+		);
+
+		return normalize( cross( du, dv ) ) ;
+	}
+
+};
+
+class KleinBottle : public Surface {
+
+public:
+
+	float a, b, c;
+
+	float transU (float u) {
+		return u * 2.0 * M_PI;
+	}
+	float transV (float v) {
+		return v * 2.0 * M_PI;
+	}
+
+	vec3 surfPos (float u, float v) {
+
+		float _a = a * cosf (u) * (1.0 + sinf (u));
+		float _b = b * sinf (u);
+		float _c = c * (1.0 - cosf (u) / 2.0);
+
+		float fx = u >= M_PI ? (_a + _c * cosf (v + M_PI)) : (_a + _c * cosf (u) * cosf (v));
+		float fy = u >= M_PI ? (_b) : (_b + _c * sinf (u) * cosf (v));
+		float fz = _c * sinf (v);
+
+		return vec3 (fx, fy, fz);
+	}
+	vec3 surfNormal (float u, float v) {
+
+		vec3 du, dv;
+
+		if (u >= M_PI) {
+
+			// derivative of a * cos (u) * (1 + sin (u)) + ((c * (1 - cos (u) / 2)) * cos (v + pi)) with respect to u/v
+			float dXdU = -a * sinf (u) * (sinf (u) + 1.0)
+				+ a * cosf (u) * cosf (u)
+				+ c * (1.0 - cosf (u) / 2.0) * cosf (u) * cosf (v)
+				+ 0.5 * c * sinf (u) * sinf (u) * cosf (v);
+			float dXdV = 0.5 * c * sinf (u) * (cosf (u) - 2.0) * sinf (v);
+			float dYdU = b * cosf (u);
+			float dYdV = 0;
+			float dZdU = 0.5 * c * sinf (u) * sinf (v);
+			float dZdV = 0.5 * c * (cosf (u) - 2.0) * cosf (v);
+
+			du = vec3 (dXdU, dYdU, dZdU);
+			dv = vec3 (dXdV, dYdV, dZdV);
+
+		} else {
+
+			// derivative of a * cos (u) * (1 + sin (u)) + ( (c * (1 - cos (u) / 2)) * cos (u) * cos (v) ) with respect to u/v
+			float dXdU = -sinf (u) * (a * sinf (u) + a + c * cosf (v))
+				+ a * powf (cosf (u), 2.0) + c * sinf (u) * cosf (u) * cosf (v);
+			float dXdV = 0.5 * c * (cosf (u) - 2.0) * cosf (u) * sinf (v);
+			float dYdU = b * cosf (u) - 0.5 * c * (cosf (u) - 2.0) * cosf (u) * cosf (v)
+				+ 0.5 * c * powf (sinf (u), 2.0) * cosf (v);
+			float dYdV = 0.5 * c * sinf (u) * (cosf (u) - 2.0) * sinf (v);
+			float dZdU = 0.5 * c * sinf (u) * sinf (v);
+			float dZdV = 0.5 * c * (cosf (u) - 2.0) * cosf (v);
+
+			du = vec3 (dXdU, dYdU, dZdU);
+			dv = vec3 (dXdV, dYdV, dZdV);
+
+		}
+
+		return normalize( cross( du, dv ) );
+	}
+
+	vec3 surfTangent (float u, float v) {
+		vec3 du;
+
+		if (u >= M_PI) {
+
+			// derivative of a * cos (u) * (1 + sin (u)) + ((c * (1 - cos (u) / 2)) * cos (v + pi)) with respect to u/v
+			float dXdU = a * powf (cosf (u), 2.0)
+				- 0.5 * sinf (u) * (2.0 * a * (sinf (u) + 1.0) + c * cosf (v));
+			float dYdU = b * cosf (u);
+			float dZdU = 0.5 * c * sinf (u) * sinf (v);
+
+			du = vec3 (dXdU, dYdU, dZdU);
+
+		} else {
+
+			// derivative of a * cos (u) * (1 + sin (u)) + ( (c * (1 - cos (u) / 2)) * cos (u) * cos (v) ) with respect to u/v
+			float dXdU = -sinf (u) * (a * sinf (u) + a + c * cosf (v))
+				+ a * powf (cosf (u), 2.0) + c * sinf (u) * cosf (u) * cosf (v);
+			float dYdU = b * cosf (u) - 0.5 * c * (cosf (u) - 2.0) * cosf (u) * cosf (v)
+				+ 0.5 * c * powf (sinf (u), 2.0) * cosf (v);
+			float dZdU = 0.5 * c * sinf (u) * sinf (v);
+
+			du = vec3 (dXdU, dYdU, dZdU);
+
+		}
+
+		return normalize( du ) ;
+	}
+
+};
+
+inline vec4 sunQuaternion (float t) {
+	return vec4 (
+		cosf (t / 4.0),
+		sinf (t / 4.0) * cosf (t) / 2.0,
+		sinf (t / 4.0) * sinf (t) / 2.0,
+		sinf (t / 4.0) * sqrtf (3.0 / 4.0)
+	);
+}
 
 struct Game {
 
@@ -563,7 +905,8 @@ public:
 Texture* texture;
 Material* material;
 Camera* camera;
-Sphere* sphere;
+// DiniSurface* sphere;
+std::vector <Drawable*> drawables;
 Shader* shader;
 std::vector <Light*> lights;
 
@@ -584,24 +927,58 @@ void onInitialization() {
 	material->shininess = 150.0;
 
 	camera = new Camera ();
-	camera->position = vec3 (0.0, 0.0, -10.0);
+	camera->position = vec3 (0.0, 0.0, -40.0);
 	camera->target = vec3 (0.0, 0.0, 0.0);
 	camera->up = vec3 (0.0, 1.0, 0.0);
 
-	sphere = new Sphere ();
-	sphere->Init ();
-	sphere->translate = vec3 (0.0, 0.0, 0.0);
-	sphere->rotationAxis = vec3 (0.0, 1.0, 0.0);
-	sphere->rotation = 0.0;
-	sphere->scale = vec3 (1.0, 1.0, 1.0);
-	sphere->texture = texture;
-	sphere->material = material;
+	KleinBottle* kb = new KleinBottle ();
+	kb->a = 6.0;
+	kb->b = 16.0;
+	kb->c = 4.0;
+	kb->Init ();
+	kb->translate = vec3 (0.0, 0.0, 0.0);
+	kb->rotationAxis = vec3 (0.0, 1.0, 0.0);
+	kb->rotation = 0;
+	kb->scale = vec3 (1.0, 1.0, 1.0);
+	kb->texture = texture;
+	kb->material = material;
+	drawables.push_back (kb);
+
+
+	for (int i = 0; i < 15; i++) {
+		DiniSurface* ds = new DiniSurface ();
+		ds->Init ();
+		float u = kb->transU (frand ());
+		float v = kb->transV (frand ());
+		ds->translate = kb->surfPos (
+			u, v
+		);
+		vec4 q = orient (
+			DINI_FORWARD,
+			kb->surfNormal (
+			 	u, v
+			) * -1.0,
+			kb->surfTangent (
+				u, v
+			)
+		);
+		ds->rotationQuaternion = q;
+		ds->rotationAxis = vec3 (0.0, 1.0, 0.0);
+		ds->rotation = 0.0;
+		ds->useQuaternion = false;
+		ds->scale = vec3 (2.0, 2.0, 2.0);
+		ds->texture = texture;
+		ds->material = material;
+		drawables.push_back (ds);
+	}
 
 	shader = new PhongShader ();
-	sphere->shader = shader;
+	for (auto &d : drawables) {
+		d->shader = shader;
+	}
 
 	Light* light = new Light ();
-	light->position = vec4 (5.0, 5.0, 4.0, 0.0);
+	light->position = vec4 (2.0, -2.0, -4.0, 0.0);
 	light->Le = vec3 (2.5, 2.5, 2.5);
 	light->La = vec3 (0.5, 0.2, 0.2);
 	lights.push_back (light);
@@ -613,15 +990,18 @@ void onDisplay() {
 	glClearColor(0.5f, 0.5f, 0.8f, 1.0f);							// background color 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the screen
 	
-	RenderState state;
-	for (auto &l : lights) {
-		state.lights.push_back (l);
+	// sphere->Draw (state);
+	for (auto &d : drawables) {
+		RenderState state;
+		for (auto &l : lights) {
+			state.lights.push_back (l);
+		}
+		state.view = camera->GetViewMatrix ();
+		state.projection = camera->GetProjectionMatrix ();
+		state.cameraPosition = camera->position;
+		state.cameraDirection = normalize (camera->target - camera->position);
+		d->Draw (state);
 	}
-	state.view = camera->GetViewMatrix ();
-	state.projection = camera->GetProjectionMatrix ();
-	state.cameraPosition = camera->position;
-	state.cameraDirection = normalize (camera->target - camera->position);
-	sphere->Draw (state);
 
 	glutSwapBuffers();									// exchange the two buffers
 }
@@ -642,6 +1022,12 @@ void onMouseMotion(int pX, int pY) {
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
 	float time = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+
+	camera->position = vec3 (
+		sinf (time) * 40.0,
+		0.0,
+		cosf (time) * 40.0
+	);
 
 	glutPostRedisplay();
 }
